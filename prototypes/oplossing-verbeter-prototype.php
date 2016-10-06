995d414cff681705f9e6ea5e961a3d6d68d4ae23
Jesse <spificator@hotmail.com>
2016-10-06 16:25:20 +0200

    toevoegen oplossing verbeter

---
+++ prototypes/oplossing-verbeter-prototype.php
@@ -0,0 +1,18 @@
+<?php
+	$tekst 			= 	'Test 123...';
+	$_2_tekst 		= 	'Test 456';
+	$_tekst 		= 	'test';
+	$zin 			= 	"Jean D'Haeseleir gaat naar Praag!!";
+/*					 of 'Jean D\'Haeseleir gaat naar Praag!!';	*/
+	$beschrijving 	= 	"Vogelbekdier";
+?>
+
+<!DOCTYPE html>
+<html>
+<head></head>
+<body>
+
+	<h1>Foutjes verbeteren</h1>
+
+</body>
+</html>
