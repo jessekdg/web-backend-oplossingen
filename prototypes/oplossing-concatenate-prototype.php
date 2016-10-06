3066910218a2e4cda39bb32551219bd4916daeca
Jesse <spificator@hotmail.com>
2016-10-06 16:37:45 +0200

    toevoegen oplossing string concatenate

---
+++ prototypes/oplossing-concatenate-prototype.php
@@ -0,0 +1,8 @@
+<?php
+
+	$voornaam 		= 	"Jesse"							;
+	$achternaam 	= 	"Op de Beeck"					;
+	$volledigeNaam	=	$voornaam . " " . $achternaam	;
+
+	echo $volledigeNaam . "<br>" . strlen($volledigeNaam);
+?>
