35e32508529bf13a784932cf344ec29a66f7b569
Jesse OdB <jesse.opdebeeck@student.kdg.be>
2016-10-12 21:52:10 +0200

    looping statements for NIET AF, NOG HULP VRAGEN

---
+++ looping-statements-for.php
@@ -0,0 +1,26 @@
+<?php
+
+	$rijen = 10;
+	$kolommen = 10;
+
+	for($i = 0 ; $i < $rijen ; $i++)
+	{
+		/* voeg rij toe */
+		for($i = 0 ; $i < $kolommen ; $i++)
+		{
+			/* Voeg kolom toe */
+		}
+	}
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Oplossing looping statements - for</title>
+	</head>
+	<body>
+		<h1>Looping statements - for</h1>
+		<p></p>
+	</body>
+</html>
