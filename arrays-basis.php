d51dcadf472b8fee58f868d6ae4e9e3c92d50e70
Jesse OdB <jesse.opdebeeck@student.kdg.be>
2016-10-12 20:59:20 +0200

    oplossing arrays basis + template gemaakt voor gemakkelijkheid

---
+++ arrays-basis.php
@@ -0,0 +1,44 @@
+<?php
+
+	/* Manier 1 
+	$dieren[0] = "Aap";
+	$dieren[1] = "Aap twee";
+	$dieren[2] = "Nog een aap";
+	$dieren[3] = "Alweer een aap";
+	$dieren[4] = "Aapje";
+	$dieren[5] = "Zoveelste aap";
+	$dieren[6] = "Aap in het kwadraat";
+	$dieren[7] = "Apekool";
+	$dieren[8] = "Apenootjes";
+	$dieren[9] = "Vogelbekdier"; 
+
+	Manier 2 */
+	$dieren = array("Aap", 
+					"Aap twee", 
+					"Nog een aap", 
+					"Alweer een aap", 
+					"Aapje", 
+					"Zoveelste aap", 
+					"Aap in het kwadraat", 
+					"Apekool", 
+					"Apenootjes", 
+					"Vogelbekdier");
+
+	$voertuigen["Landvoertuigen"] = array("Driewieler", "Zeepkist");
+	$voertuigen["Watervoertuigen"] = array("Boot");
+	$voertuigen["Luchtvoertuigen"] = array("Helikopter", "Vliegtuig");
+
+	var_dump($voertuigen);
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Arrays basis</title>
+	</head>
+	<body>
+		<h1>Deel 1</h1>
+		<p></p>
+	</body>
+</html>
---
+++ template.php
@@ -0,0 +1,16 @@
+<?php
+
+
+
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>TITEL VAN OPDRACHT</title>
+	</head>
+	<body>
+		<h1>Deel 1</h1>
+		<p></p>
+	</body>
+</html>
