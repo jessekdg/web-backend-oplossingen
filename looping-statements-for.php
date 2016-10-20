5faa60ff436a370a9a97a07ab6a55cb73da4d3e7
Jesse OdB <Jesse OdB>
2016-10-20 15:09:51 +0200

    toevoegen oplossing looping statements for

--- looping-statements-for.php
+++ looping-statements-for.php
@@ -3,15 +3,24 @@
 	$rijen = 10;
 	$kolommen = 10;
 
+
+	/* onderstaande code is de basis, maar omdat er effectief html code geinjecteerd moet
+	worden, zetten we dit in de html code.
+	Op de plaatsen waar een rij/kolom toegevoegd moet worden, zetten we gewoon de nodige html code
+	(tr of td).
+	NODIG: php code in html afsluiten met ?> na de eerste akolade van de for loop, en nieuwe php code block
+	starten om de sluitende akolade in te voegen.
+
+	
 	for($i = 0 ; $i < $rijen ; $i++)
 	{
-		/* voeg rij toe */
+		voeg rij toe
 		for($i = 0 ; $i < $kolommen ; $i++)
 		{
-			/* Voeg kolom toe */
+			
 		}
 	}
-
+*/
 ?>
 
 <!DOCTYPE html>
@@ -21,6 +30,18 @@
 	</head>
 	<body>
 		<h1>Looping statements - for</h1>
-		<p></p>
+		<table>
+
+		<?php for ($i = 0; $i < $rijen; $i++) { ?>
+				<tr>
+
+					<?php for ($j = 0; $j < $kolommen; $j++) { ?>
+						<td>kolom</td>
+					<?php } ?>
+
+				</tr>
+		<?php } ?>
+		
+		</table>
 	</body>
 </html>
