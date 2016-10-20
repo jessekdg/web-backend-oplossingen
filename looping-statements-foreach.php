bd51b17705961f0029baf7c9dda6435504526c79
Jesse OdB <Jesse OdB>
2016-10-20 16:02:34 +0200

    toevoegen oplossing looping statements foreach (eindelijk)

--- looping-statements-foreach.php
+++ looping-statements-foreach.php
@@ -1,9 +1,23 @@
 <?php
 
-	$text = file_get_contents(text-file.txt);
+	$text = file_get_contents("text-file.txt");
 	$textChars = str_split($text, 1);
-	$textChars = sort($textChars);
+	sort($textChars);
 	$textChars = array_reverse($textChars);
+
+	$textCharsCounted = array_count_values($textChars);
+
+	$aantalVerschillende = 0;
+	$huidigCharacter = null;
+
+	foreach($textCharsCounted as $character)
+	{
+		if($character != $huidigCharacter)
+		{
+			$huidigCharacter = $character;
+			$aantalVerschillende++;
+		}
+	}
 
 ?>
 
@@ -14,6 +28,13 @@
 	</head>
 	<body>
 		<h1>Looping statements - foreach</h1>
-		<p></p>
+		<p>Er zijn <?= $aantalVerschillende ?> verschillende characters.</p>
+
+		<?php 
+		foreach($textCharsCounted as $character => $aantalIteraties)
+		{ ?>
+		<p><?= $character . " komt " . $aantalIteraties . " keer voor." ?></p>
+		<?php
+		} ?>
 	</body>
 </html>
