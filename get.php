bbc41fe44452895cd7466a9c4d88d6935a6e70c8
Jesse OdB <jesse.opdebeeck@student.kdg.be>
2016-10-23 15:55:45 +0200

    basis html get oefening klaar

--- get.php
+++ get.php
@@ -1,9 +1,9 @@
 <?php
 
-	$nieuws = array();
+	$artikels = array();
 
 	/* http://www.hln.be/hln/nl/957/Binnenland/article/detail/2934501/2016/10/21/Explosie-op-militair-domein-in-Helchteren-blijkt-oefening.dhtml */
-	$nieuws[0] = 
+	$artikels[0] = 
 	array(
 		'Explosie op militair domein in Helchteren blijkt oefening',
 		'Eerder werd melding gemaakt van een explosie op het militair domein in Helchteren. Maar nu blijkt dat een vliegtuig tijdens een oefening door de geluidsmuur is geknald. Van de zeven gewonde militairen waarover eerder werd bericht, blijkt geen sprake. De knal moet wel enorm zijn geweest, aangezien mensen tot in Kiewit melding maakten.',
@@ -12,7 +12,7 @@
 	);
 	
 	/* http://www.hln.be/hln/nl/960/Buitenland/article/detail/2934291/2016/10/21/Russische-oorlogsvloot-rukt-op-naar-het-Kanaal-Britse-marine-volgt-op-de-voet.dhtml */
-	$nieuws[1] = 
+	$artikels[1] = 
 	array(
 		'Russische oorlogsvloot rukt op naar het Kanaal, Britse marine volgt op de voet',
 		'Een konvooi Russische oorlogsschepen dat onderweg is naar de Middellandse Zee, zal door het Kanaal varen en wordt daarbij op de voet gevolgd door de Britse marine, daarbij ondersteund door de luchtmacht.',
@@ -21,7 +21,7 @@
 	);
 
 	/* http://www.hln.be/hln/nl/957/Binnenland/article/detail/2934273/2016/10/21/Politierechter-tegen-Turkse-Kan-jij-na-20-jaar-nog-geen-Nederlands.dhtml */
-	$nieuws[2] = 
+	$artikels[2] = 
 	array(
 		'Politierechter tegen Turkse: "Kan jij na 20 jaar nog altijd geen Nederlands?"',
 		'Laat er geen misverstand over bestaan: wie voor de politierechter verschijnt, heeft vermoedelijk iets mispeuterd. Meestal eigen schuld dikke bult, maar soms ook een ongelukkige samenloop van omstandigheden, gewoon brute pech of wordt de onschuld bewezen. Niet zelden proberen overtreders hun misstap te vergoelijken met op zijn minst merkwaardige uitvluchten. Onze reporters tekenden de voorbije week in de Vlaamse politierechtbanken enkele zaken op die dit ten overvloede illustreren.',
@@ -38,6 +38,20 @@
 	</head>
 	<body>
 		<h1>Deel 1</h1>
-		<pre><?= var_dump($nieuws) ?></pre>
+		<pre><?= var_dump($artikels) ?></pre>
+
+		<?php foreach($artikels as $key => $value): ?>
+			<form>
+				<fieldset>
+					<legend><b><?= $artikels[$key][0] ?></b></legend>
+					<img
+						src = <?= $artikels[$key][2] ?>,
+						alt = <?= $artikels[$key][3] ?>,
+						height = 150px, width = 500px>
+					<p><?= $artikels[$key][1] ?></p>
+				</fieldset>>
+			</form>
+		<?php endforeach; ?>
+
 	</body>
 </html>
