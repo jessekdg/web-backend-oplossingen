96c1ca630a6dad06ef0f55e5d57483a457fc7e5d
jesse <jesse.opdebeeck@student.kdg.be>
2016-11-24 15:06:17 +0100

    cookies verder uitgewerkt, deel 2 af

--- OPL_Cookies/cookies.php
+++ OPL_Cookies/cookies.php
@@ -9,12 +9,29 @@
 	{
 		if($_POST['gebruikersnaam'] == $gegevens[0] && $_POST['paswoord'] == $gegevens[1])
 		{
+			$loginSuccesful = true;
 			$message = 'inloggen gelukt';
+			if( isset($_POST['onthoudmij']) )
+			{
+				setcookie('succesfulLogin', true, time() + 2592000);
+			}
+			else
+			{
+				setcookie('succesfulLogin', true);
+			}
+			header('location: cookies.php');
 		}
 		else
 		{
 			$message = 'inloggen mislukt';
+			$loginSuccesful = false;
 		}
+	}
+
+	if( isset($_GET['logout']) )
+	{
+		setcookie('succesfulLogin', false);
+		unset($_COOKIE['succesfulLogin']);
 	}
 
 ?>
@@ -25,6 +42,16 @@
 		<title>Cookies</title>
 	</head>
 	<body>
+	<?php if( isset($_COOKIE['succesfulLogin']) ): ?>
+		<h1>Dashboard</h1>
+		<p>U bent ingelogd.</p>
+			<!-- TEST COOKIE COUNT
+			<?php if( isset($_COOKIE['succesfulLogin']) ): ?>
+				<p>Cookie set: <?= $_COOKIE['succesfulLogin'] ?></p>
+			<?php endif; ?>
+			-->
+		<a href='cookies.php?logout=1'>Uitloggen</a>
+	<?php else: ?>
 		<h1>Inloggen</h1>
 		<p><?= $message ?></p>
 		<form method='POST' action='#'>
@@ -38,7 +65,10 @@
 					<input type='password' name='paswoord' id='paswoord'>
 				</li>
 			</ul>
+			<input type='checkbox' name='onthoudmij' id='onthoudmij'>Onthoud mij
+			<br>
 			<input type='submit' value='Verzenden' name='verzenden'>
 		</form>
+	<?php endif; ?>
 	</body>
 </html>
